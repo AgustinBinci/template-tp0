4c43745f55407c87165a44c6be71e7a36282c39f
AgustinBinci <agustinbinci@gmail.com>
2016-03-21 20:23:43 -0300

    Elimino clases que habian quedado

--- src/main/java/ar/fiuba/tdd/template/tp0/IRegularExpresion.java
+++
@@ -1,9 +0,0 @@
-package ar.fiuba.tdd.template.tp0;
-
-/**
- * Created by Monica2015 on 19/3/16.
- */
-public interface IRegularExpresion {
-    Boolean isRegularExpression(String aText);
-    String getRandomStringForWithLimit(Character aCharacter, Integer aLimit);
-}
